78e4f51c7565e2dd5e246e1d62cd24a604881dd6
ahabchan <karimahabchan@gmail.com>
2026-05-25 13:00:13 +0200

    fix and refactor

--- pwm_driver.c
+++ pwm_driver.c
@@ -1,26 +1,30 @@
 //this is .c file for PWM driver BY DEFAULT IT USES PORTC PIN 2
+
+#include "pwm_driver.h"
+#include <xc.h>
+
 void pwm_init(){
 //SET PIN IN PROPER MODE
-      CCP1CON.P1M0=CLEAR;
-      CCP1CON.P1M1=SET;
+      CCP1CONbits.P1M0=CLEAR;
+      CCP1CONbits.P1M1=SET;
       
-      CCP1CON.CCP1M2=SET;
-      CCP1CON.CCP1M3=SET;
+      CCP1CONbits.CCP1M2=SET;
+      CCP1CONbits.CCP1M3=SET;
       
-      PIR1.TMR2IF=CLEAR;
-      T2CON.TMR2ON=SET;
-      TRISC.B2=CLEAR;
+      PIR1bits.TMR2IF=CLEAR;
+      T2CONbits.TMR2ON=SET;
+      TRISC2=CLEAR;
       
       pwm_period(256);
       //CALCULATING POWER IN PWM TERMS
       pulse_width(50);
 }
 
-void pulse_width(int width){
+void pulse_width(unsigned int width){
       CCP1CON|=((width&0x0003)<<4);
       CCPR1L=width>>2;
 }
 
-void pwm_period(int period){
+void pwm_period(unsigned int period){
       PR2=period-1;
 }
--- pwm_driver.h
+++ pwm_driver.h
@@ -1,10 +1,13 @@
 //this is .h file for PWM driver BY DEFAULT IT USES PORTC PIN 2
 
+#ifndef PWM_DRIVER_H
+#define PWM_DRIVER_H
+
 #define CLEAR 0
 #define SET 1
 
 void pwm_init();
-void pulse_width(int width); //1023 bits de resolucion
-void pwm_period(int period);
+void pulse_width(unsigned int width); //resolution of 1023 bits 
+void pwm_period(unsigned int period);
 
-#include "pwm_driver.c"
+#endif
