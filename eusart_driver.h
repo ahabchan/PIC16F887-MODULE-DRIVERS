e020c1c620aa073def178ad4959b19a0461ddb5c
ahabchan <karimahabchan@gmail.com>
2026-05-25 11:48:26 +0200

    fix and refactor

--- eusart_driver.h
+++ eusart_driver.h
@@ -1,5 +1,7 @@
-
 //this is .h file for eusart driver BY DEFAULT IT IS SET TO 2400 BAUD
+#ifndef EUSART_DRIVER_H
+#define EUSART_DRIVER_H
+
 #define SET 1
 #define CLEAR 0
 #define END '/' //USE THIS CHARACTER FOR SET THE LAST CHARACTER OF A STRING
@@ -9,10 +11,10 @@ unsigned char received_data=0;
 unsigned char numb[]={0,0,0};
 
 //void interrupt(); // if you are using more interrupts, put this inside the general interrupt function
-void eusart_init(char SYNC_MODE, char H_SPEED);
+void eusart_init();
 void eusart_send(unsigned char *package);
 void eusart_sendn(unsigned char number);
 void eusart_sendc(unsigned char c);
 void eusart_receiver(unsigned char choose);
 
-#include "eusart_driver.c"
+#endif
